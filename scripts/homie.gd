333cbae907e37c50b0980c48f2cbae882d37ebe1
Amaar Khan <amaar.khan02@gmail.com>
2026-04-23 00:18:57 +0100

    Add functions that do stuff

--- scripts/homie.gd
+++ scripts/homie.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+@export var wall_slide_ray_cast: RayCast2D
+@export var animated_sprite: AnimatedSprite2D
+
 enum STATE {
 	FALL,
 	FLOOR,
@@ -34,8 +37,26 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
-func switch_state():
+func switch_state(to_state: STATE):
+	var previous_state := active_state
+	active_state = to_state
+
+func process_state(delta: float):
 	pass
 
-func process_state():
-	pass
+func handle_movement(input_direction: float):
+	if input_direction == 0:
+		input_direction = signf(Input.get_axis("move_left", "move_right"))
+	set_facing_direction(input_direction)
+	velocity.x = move_toward(velocity.x, input_direction * SPEED, SPEED)
+
+func set_facing_direction(direction: float) -> void:
+	if direction:
+		animated_sprite.flip_h = direction < 0
+		facing_direction = direction
+		wall_slide_ray_cast.position.x = direction * absf(wall_slide_ray_cast.position.x)
+		wall_slide_ray_cast.target_position.x = direction * absf(wall_slide_ray_cast.target_position.x)
+		wall_slide_ray_cast.force_raycast_update()
+
+func can_wall_slide() -> bool:
+	return is_on_wall_only() and wall_slide_ray_cast.is_colliding()
